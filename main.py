75dc645731bbb629eadaade5e074b2db78c537d3
RileyGredvig10 <821118@mystma.org>
2022-09-13 09:41:01 -0500

    3 functions

---
+++ main.py
@@ -0,0 +1,14 @@
+def addTwoNumbers():
+   x = 69
+   y = 420
+   print(x+y)
+
+def myName():
+    print("Riley")
+
+def myFiveDreamCars():
+    print("1. Toyota AE86 Trueno 2. Nissan R32 GTR 3. Mazda RX-7 FD3S 4. 1969 Dodge Charger R/T 5. Mclaren F1")
+
+def myFamily():
+    print ("Greg(Dad) Rochelle(Mom) Gabby (Sister) Zach (Brother) ")
+    
